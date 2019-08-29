c7a476ec103c5a19cda2b7a8f5a73b7f0bc7e6fa
Mark McCann <mjmccann12@me.com>
2019-08-29 13:43:16 -0500

    added js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const journalEntry=[]
+
+const entry1 = {
+    date: 8/26/19,
+    concepts: "objects",
+    content: "Having difficultly grasping some of the concepts.",
+    mood: "confused"
+}
+
+const entry2 = {
+    date: 8/27/19,
+    concepts: "functions and logic",
+    content: "Not completely confident in what we have learned",
+    mood: "frustrated"
+}
+
+journalEntry.push(entry1)
+console.log(journalEntry)
