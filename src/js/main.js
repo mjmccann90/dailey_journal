58b3ab9c4ff5bb88683e10f81385d3095b1d1ebc
Mark McCann <mjmccann12@me.com>
2019-09-03 13:21:09 -0500

    completed journal 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
 const journalEntry=[]
 
 const entry1 = {
-    date: 8/26/19,
+    date: "8/26/19",
     concepts: "objects",
     content: "Having difficultly grasping some of the concepts.",
-    mood: "confused"
+    mood: " confused"
 }
 
 const entry2 = {
-    date: 8/27/19,
+    date: "8/27/19",
     concepts: "functions and logic",
     content: "Not completely confident in what we have learned",
-    mood: "frustrated"
+    mood: "  frustrated"
 }
 
-journalEntry.push(entry1)
+journalEntry.push(entry1, entry2)
 console.log(journalEntry)
